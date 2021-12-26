2c4bcf2cf0691656babbc00d0d51205e29f6f8a2
Your Name <ubaydullayev@samtuit.uz>
2021-12-26 21:19:09 +0500

    9-misol

--- bool09.py
+++ bool09.py
@@ -1,4 +1,5 @@
 def main(a):
+    b = a > 0
     """Check the natural number.Natural numbers are numbers used in counting.
     Args:
         a: int
@@ -6,4 +7,4 @@ def main(a):
         bool
     """
     # Write your code here
-    return 
+    return b
